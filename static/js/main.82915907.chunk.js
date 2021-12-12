b7ad9418aeefb91f180e4127652aa467006cb4b5
Gene4kaS <jannasavchenko@gmail.com>
2021-12-12 21:42:06 +0200

    Updates

--- static/js/main.82915907.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{2:function(p,s,n){"use strict";n.r(s);n(0),n(4),n(8)},8:function(p,s,n){}},[[2,1,2]]]);
-//# sourceMappingURL=main.82915907.chunk.js.map
